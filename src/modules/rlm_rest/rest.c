2a082a406aaf4555b6988ee51693be57440071c5
Durand Fabrice <oeufdure@gmail.com>
2025-03-21 11:08:12 -0400

    Fixes vp_prints_value_json

--- src/modules/rlm_rest/rest.c
+++ src/modules/rlm_rest/rest.c
@@ -848,7 +848,7 @@ static size_t rest_encode_json(void *out, size_t size, size_t nmemb, void *userd
 				 *  write that out.
 				 */
 				attr_space = fr_cursor_next_peek(&ctx->cursor) ? freespace - 1 : freespace;
-				len = vp_prints_value_json(p, attr_space + 1, vp);
+				len = vp_prints_value_json(p, attr_space + 1, vp, ctx->section->raw_value);
 				if (is_truncated(len, attr_space + 1)) goto no_space;
 
 				/*
