706197cb252e6f27862af75d3dd5daedc54e2571
Durand Fabrice <oeufdure@gmail.com>
2025-03-21 22:00:32 -0400

    Test

--- src/lib/print.c
+++ src/lib/print.c
@@ -681,7 +681,7 @@ size_t vp_prints_value_json(char *out, size_t outlen, VALUE_PAIR const *vp, bool
     case PW_TYPE_IPV4_ADDR:
         ip_ntoa(buffer, vp->vp_ipaddr);
         snprintf(out, outlen, "\"%s\"", buffer); // Use snprintf to add quotes
-        p += strlen(out);
+        p += strlen(out) - 1;
 
         break;
 
